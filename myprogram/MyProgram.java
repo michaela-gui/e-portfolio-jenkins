576c1ce1c63825f261e3fda88ec68901f5bac26a
michaela-gui <56273662+michaela-gui@users.noreply.github.com>
2020-05-20 09:17:15 +0200

    Update myprogram

--- myprogram/MyProgram.java
+++ myprogram/MyProgram.java
@@ -4,6 +4,6 @@ public class MyProgram
     {
         // Ausgabe Hello World!
         System.out.println("Hello World!");
-        System.out.println("Hello World2!");
+        System.out.println("Hello World 2!");
     }
 }
